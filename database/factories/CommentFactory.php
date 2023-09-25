ffc770b5e99fea8d503aa1919dbbb563924bcb20
Luke Downing <lukeraymonddowning@gmail.com>
2023-09-25 10:20:05 +0100

    Episode 4.

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'post_id' => Post::factory(),
+            'body' => fake()->realText(250),
+        ];
+    }
+}
