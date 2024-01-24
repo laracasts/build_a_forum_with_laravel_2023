5a45ae20ab99d775211beb80e81ae4b051542ad6
lukeraymonddowning <lukeraymonddowning@gmail.com>
2024-01-24 09:57:59 +0000

    Episode 37

--- resources/js/Components/Pill.vue
+++ resources/js/Components/Pill.vue
@@ -2,7 +2,7 @@
     <Link
         :href="href"
         class="whitespace-nowrap mb-2 rounded-full border border-pink-500 px-2 py-0.5 text-pink-500 hover:bg-indigo-500 hover:text-indigo-50"
-        :class="{ 'bg-indigo-500 text-indigo-50': filled }"
+        :class="{ 'bg-indigo-500 !text-indigo-50': filled }"
     >
         <slot></slot>
     </Link>
